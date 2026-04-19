3840563955430b221cbf133b6fad0f49dfaa9baf
JessVakk <92309653+JessVakk@users.noreply.github.com>
2026-04-19 15:32:17 +0200

    add income transactions and update balance

--- src/script.js
+++ src/script.js
@@ -2,7 +2,9 @@ const descInput = document.getElementById("desc");
 const amountInput = document.getElementById("amount")
 const incomeBtn = document.getElementById("incomeBtn");
 const incomeList = document.getElementById('incomeList');
+const balanceSaldo = document.getElementById('balance');
 
+let balance = 0;
 
 incomeBtn.addEventListener("click", function(){
     if(amountInput.value.trim()===''|| descInput.value.trim()===''){
@@ -12,19 +14,23 @@ incomeBtn.addEventListener("click", function(){
     const amount = Number(amountInput.value);
 
     if(isNaN(amount)) {
-         console.log("Not av valid input");
-         return;  
-    }
+         return; }
+
+    const li = document.createElement("li");
+    li.textContent = `${descInput.value} - ${amountInput.value} kr (inkomst)`;
+    incomeList.appendChild(li);
+    console.log(li);
+
+    balance += amount;
+    balanceSaldo.textContent = balance;
+    console.log(balance);
+
     console.log(descInput.value);
     console.log(amount);
     console.log(amountInput.value);
-
-    const li = document.createElement("li");
-    li.textContent = `${descInput.value} - ${amountInput.value} kr`;
-    incomeList.appendChild(li);
-    console.log(li);
+    }   
     
-});
+);
 
 
 
